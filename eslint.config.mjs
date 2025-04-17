a3a6d580414346d8eb66247f1b89d7ee77912151
Robert Garofalo <r.s.garofalo87@gmail.com>
2025-04-17 14:53:12 +1000

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  {
-    ignores: ["prisma/generated/**"],
-  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
