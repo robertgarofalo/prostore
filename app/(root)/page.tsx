10217cf20bc3d0c6936df5bb919676674d135caa
Robert Garofalo <r.s.garofalo87@gmail.com>
2025-04-17 14:30:01 +1000

    Initial deployment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+// import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+
+const Homepage = async () => {
+const latestProducts = await getLatestProducts()
+
+  return (
+    <>
+    <ProductList data={latestProducts} title="Newest arrivals" limit={4}/>
+    </>
+  )
+}
+ 
+export default Homepage;
